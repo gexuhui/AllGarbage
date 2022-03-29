f8634e12f54ee17bdacea087571f1dd226005b6b
戈旭辉 <gexuhui@bmac.com.cn>
2022-03-29 12:03:11 +0800

    Initial Commit

---
+++ AllEmbracing/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AllEmbracing
+//
+//  Created by gexuhui on 2022/3/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
